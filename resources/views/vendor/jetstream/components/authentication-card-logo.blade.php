79bcef4d74abf1ddc12d9563a4c6f1b50b871308
Ian Maclagan <iwmaclagan@gmail.com>
2020-10-14 17:04:16 +1100

    added names to register and login forms

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,6 +1,6 @@
 <a href="/">
-<div class="inline-block rounded-full bg-teal-900">
-<svg class="h-16 w-16" version="1.1" viewBox="0 0 .81444 1.1537" xmlns="http://www.w3.org/2000/svg">
+    <div class="inline-block rounded-full bg-teal-900">
+        <svg class="h-12 w-12 sm:h-16 sm:w-16" version="1.1" viewBox="0 0 .81444 1.1537" xmlns="http://www.w3.org/2000/svg">
             <g transform="translate(-6.9426 -13.633)">
                 <text fill="#000000" font-family="sans-serif" font-size="10.583px" style="line-height:1.25;shape-inside:url(#rect957);white-space:pre" xml:space="preserve" />
                 <path d="m7.3499 13.635c-0.13455 0.34027-0.40651 0.44762-0.40651 0.44762s0.047047 0.35858 0.40651 0.70336c0.36398-0.34478 0.40647-0.70336 0.40647-0.70336s-0.27353-0.10735-0.40647-0.44762zm0.011167 0.34924a0.15645 0.23187 0 0 1 0.15645 0.23185 0.15645 0.23187 0 0 1-0.15645 0.23185 0.15645 0.23187 0 0 1-0.15645-0.23185 0.15645 0.23187 0 0 1 0.15645-0.23185z" fill="#fff" stroke="#000" stroke-width=".0013435px" />
@@ -10,5 +10,6 @@
                 </g>
             </g>
         </svg>
-</div>
+    </div>
+    <div>{{ $slot }}</div>
 </a>
